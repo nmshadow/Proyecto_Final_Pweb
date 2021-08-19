2038ed0c5d9287768f2ce7799109484796342011
Randy Taco <srtacoa@unac.edu.pe>
2021-08-18 23:38:15 -0500

    issue-n15close1
    implementacion del modo oscuro al NAV y body

--- app.js
+++ app.js
@@ -22,6 +22,30 @@ const navSlide = () => {
 navSlide();
 
 
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+	if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+	} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+}
+
+
+
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
